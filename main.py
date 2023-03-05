e9556d24e0e33fbf37663ebb2d8034902a1ea375
MuhammedAli <azimov.mehemmedeli@mail.ru>
2023-03-05 17:42:18 +0400

    03.05.2023 17:42

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import methods
+import time
 
 conn = methods.get_connection()
+driver = methods.get_driver("about:blank")
+
 
 for brand in methods.brands:
-    print(brand[0], brand[1])
-    main_url = f'''https://turbo.az/autos?q%5Bsort%5D=&q%5Bmake%5D%5B%5D={brand[0]}&q%5Bmodel%5D%5B%5D=&q%5Bused%5D=&q%5Bregion%5D%5B%5D=&q%5Bprice_from%5D=&q%5Bprice_to%5D=&q%5Bcurrency%5D=azn&q%5Bloan%5D=0&q%5Bbarter%5D=0&q%5Bcategory%5D%5B%5D=&q%5Byear_from%5D=&q%5Byear_to%5D=&q%5Bcolor%5D%5B%5D=&q%5Bfuel_type%5D%5B%5D=&q%5Bgear%5D%5B%5D=&q%5Btransmission%5D%5B%5D=&q%5Bengine_volume_from%5D=&q%5Bengine_volume_to%5D=&q%5Bpower_from%5D=&q%5Bpower_to%5D=&q%5Bmileage_from%5D=&q%5Bmileage_to%5D=&q%5Bonly_shops%5D=&q%5Bprior_owners_count%5D%5B%5D=&q%5Bseats_count%5D%5B%5D=&q%5Bmarket%5D%5B%5D=&q%5Bcrashed%5D=1&q%5Bpainted%5D=1&q%5Bfor_spare_parts%5D=0'''
-    main_driver = methods.get_driver(main_url)
+    # print(brand[0], brand[1])
+    main_url = methods.get_url_for_brand(brand)
+    methods.open_tab(driver, main_url)
+    methods.switch_tab(driver, 1)
+    methods.close_tab(driver, 0)
+    methods.switch_tab(driver, 0)
 
-    cars = main_driver.find_elements(By.CLASS_NAME, 'products-i__link')
-    for car in cars:
-        car_link = car.get_attribute('href')
-        details_driver = methods.get_driver(car_link)
-        car_details_price = details_driver.find_elements(By.CLASS_NAME, 'product-sidebar__box')
-        for car_price in car_details_price:
-            price = car_price.find_element(By.CLASS_NAME, 'product-price')
-        
-        car_details = details_driver.find_elements(By.CLASS_NAME, 'product-properties__i-value')
-        for car_detail in car_details:
-            detail = car_detail.text   
-            print(detail)
-            sql = f'''insert into cars (NAME) values (N'{detail}')'''
-            cursor = conn.cursor()
-            cursor.execute(sql)
-            conn.commit()
+    while True:
+        cars = driver.find_elements(By.CLASS_NAME, 'products-i__link')
+        for car in cars:
+            car_link = car.get_attribute('href')
+            driver.execute_script(f"window.open('{car_link}');")
+            driver.switch_to.window(driver.window_handles[len(driver.window_handles) - 1])
+            # time.sleep(3)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[len(driver.window_handles) - 1])
+            
+            # car_details_price = details_driver.find_elements(By.CLASS_NAME, 'product-sidebar__box')
+            # for car_price in car_details_price:
+            #     price = car_price.find_element(By.CLASS_NAME, 'product-price')
+            
+            # car_details = details_driver.find_elements(By.CLASS_NAME, 'product-properties__i-value')
+            # for car_detail in car_details:
+            #     detail = car_detail.text   
+            #     print(detail)
+            #     sql = f'''insert into cars (NAME) values (N'{detail}')'''
+            #     cursor = conn.cursor()
+            #     cursor.execute(sql)
+            #     conn.commit()
+        try:
+            next_button = driver.find_element(By.LINK_TEXT, 'Növbəti')
+            next_button.click()
+        except NoSuchElementException:
+            break
+
+    
+
+
+
+
+
+
+
+
 
 
 
